3cb18e2e9c51fb4ce9c84cb093cd7d35cb4563e4
Stijn Rommens <113170925+stijnrommens@users.noreply.github.com>
2026-05-08 13:50:16 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name="pioreactor-ph-reading",
+    version="0.1.0",
+    license="MIT",
+    description="Read pH data.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author_email="stijn.rommens@kuleuven.be",
+    author="Stijn Rommens, VIB/KU Leuven",
+    url="https://github.com/stijnrommens/pioreactor-ph-reading",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={"pioreactor.plugins": "pioreactor_relay_plugin = pioreactor_relay_plugin"},
+)
