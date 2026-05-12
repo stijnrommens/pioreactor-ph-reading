6f2276beeb5d83970c11a61f43da8ff917c9569c
Stijn Rommens <113170925+stijnrommens@users.noreply.github.com>
2026-05-12 18:11:12 +0200

    Restructure base pump code

--- pioreactor_ph_reading/base_pump.py
+++ pioreactor_ph_reading/base_pump.py
@@ -1,86 +1,98 @@
 # -*- coding: utf-8 -*-
-import json
-       
-from pioreactor.hardware import PWM_TO_PIN
-from pioreactor.utils.pwm import PWM
-from pioreactor.utils import clamp
+from __future__ import annotations
+
+import click
+from pioreactor.background_jobs.base import BackgroundJobWithDodgingContrib
+from pioreactor.cli.run import run
 from pioreactor.config import config
-from pioreactor.background_jobs.base import BackgroundJobWithDodging
-from pioreactor.whoami import get_unit_name, get_latest_experiment_name
+from pioreactor.hardware import PWM_TO_PIN
+from pioreactor.utils import clamp
+from pioreactor.utils.pwm import PWM
+from pioreactor.whoami import get_assigned_experiment_name
+from pioreactor.whoami import get_unit_name
 
-class BasePump(BackgroundJobWithDodging):
-
-    published_settings = {
-        "initial_duty_cycle": {"datatype": "float", "settable": True, "unit": "%"},
-        # "on": {"datatype": "boolean", "settable": True},
-    }
-    
+class BasePump(BackgroundJobWithDodgingContrib):
     job_name="base_pump"
+    published_settings = {"duty_cycle": {"settable": True, "unit": "%", "datatype": "float"}}
+    
+    def __init__(
+        self,
+        unit: str,
+        experiment: str,
+        duty_cycle: float,
+        hertz: float = 60,
+        enable_dodging_od: bool = False,
+    ):
+        super(BasePump, self).__init__(
+            unit=unit,
+            experiment=experiment,
+            enable_dodging_od=enable_dodging_od,
+        )
 
-    def __init__(self, hz, initial_duty_cycle, unit, experiment, start_on = True, **kwargs):
-        super().__init__(unit=unit, experiment=experiment)
-        self.hz = hz
-        self._initial_duty_cycle = initial_duty_cycle
-        self.duty_cycle = initial_duty_cycle
-            
-        self.pwm_pin = PWM_TO_PIN[config.get("PWM_reverse", "base_pump")]
-        # looks at config.ini/configuration on UI to match 
-        # changed PWM channel 3 to "base_pump" on leader
-        # whatevers connected to channel 3 will receive power 
+        self.hertz = hertz
+        try:
+            self.pin = PWM_TO_PIN[config.get("PWM_reverse", "base_pump")]
+        except KeyError:
+            raise KeyError("Unable to find `base_pump` under PWM section in the config.ini")
 
-        self.pwm = PWM(self.pwm_pin, self.hz)
-        self.pwm.lock()
-
-    def set_duty_cycle(self, new_duty_cycle):
-        self.duty_cycle = clamp(0, float(new_duty_cycle), 100)
-        self.pwm.change_duty_cycle(self.duty_cycle)
-
-    def on_init_to_ready(self):
-        self.pwm.start(self.duty_cycle)
-
-    def on_ready_to_sleeping(self):
-        self._previous_duty_cycle = self.duty_cycle
-        self.set_duty_cycle(0)
-
-    def on_sleeping_to_ready(self):
-        self.set_duty_cycle(self._previous_duty_cycle)
+        self.duty_cycle = duty_cycle
+        self.pwm = PWM(
+            self.pin, self.hertz, unit=self.unit, experiment=self.experiment, pub_client=self.pub_client
+        )
+        self.pwm.start(0)
 
     def on_disconnected(self):
-        self.logger.debug("disconnecting... will clean up PWM")
+        self.stop_pumping()
+        self.pwm.stop()
         self.pwm.clean_up()
-        
-    def action_to_do_before_od_reading(self):
-        self.set_on(False)
-    
-    def action_to_do_after_od_reading(self):
-        self.set_on(True)
-    
-import click
 
-@click.command(name="base_pump")
-@click.option(
-    "--initial-dc",
-    default=config.getfloat("base_pump", "initial_duty_cycle"),
-    show_default=True,
-    type=click.FloatRange(0, 100, clamp=True),
-)
-@click.option(
-    "--hz",
-    default=config.getfloat("base_pump", "hz"),
-    show_default=True,
-    type=click.FloatRange(1, 10_000, clamp=True),
-)
-def click_base_pump(initial_dc, hz):
+    def stop_pumping(self):
+        if hasattr(self, "pwm"):
+            self.pwm.change_duty_cycle(0)
+
+    def start_pumping(self):
+        self.pwm.change_duty_cycle(self.duty_cycle)
+
+    def on_sleeping(self):
+        self.stop_pumping()
+
+    def on_sleeping_to_ready(self) -> None:
+        self.start_pumping()
+
+    def set_duty_cycle(self, value):
+        self.duty_cycle = clamp(0, round(float(value)), 100)
+        self.pwm.change_duty_cycle(self.duty_cycle)
+
+    def action_to_do_before_od_reading(self):
+        self.stop_pumping()
+
+    def action_to_do_after_od_reading(self):
+        self.start_pumping()
+
+    def initialize_continuous_operation(self):
+        self.start_pumping()
+
+    def initialize_dodging_operation(self):
+        self.stop_pumping()
+    
+
+@run.command(name="base_pump")
+def click_base_pump():
     """
     Start the base pump
     """
-    job = BasePump(
+    dc = config.getfloat("base_pump.config", "duty_cycle")
+    hz = config.getfloat("base_pump.config", "hertz")
+    unit = get_unit_name()
+    experiment = get_assigned_experiment_name(unit)
+    enable_dodging_od = config.getboolean("air_bubbler.config", "enable_dodging_od", fallback="false")
+
+    bp = BasePump(
         hz=hz,
-        initial_duty_cycle=initial_dc,
-        unit=get_unit_name(),
-        experiment=get_latest_experiment_name(),
+        initial_duty_cycle=dc,
+        unit=unit,
+        experiment=experiment,
+        enable_dodging_od=enable_dodging_od,
     )
-    job.block_until_disconnected()
-    
-if __name__ == "__main__":
-    click_base_pump()
+    bp.start_pumping()
+    bp.block_until_disconnected()
