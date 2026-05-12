f5bf52010b8097f01878a2c81ebacfd6c66a51d0
Stijn Rommens <113170925+stijnrommens@users.noreply.github.com>
2026-05-12 16:19:16 +0200

    Add base pump to config

--- pioreactor_ph_reading/__init__.py
+++ pioreactor_ph_reading/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 from pioreactor_ph_reading.ph_reading import click_ph_reading
-# from pioreactor_ph_reading.ph_control
+from pioreactor_ph_reading.base_pump import click_base_pump
