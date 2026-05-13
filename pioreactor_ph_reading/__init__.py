eb246fe6c911c37bf39fb2e938d927e71efce38b
Stijn Rommens <113170925+stijnrommens@users.noreply.github.com>
2026-05-13 14:01:35 +0200

    Remove base pump

--- pioreactor_ph_reading/__init__.py
+++ pioreactor_ph_reading/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
-from pioreactor_ph_reading.base_pump import click_base_pump
 from pioreactor_ph_reading.ph_reading import click_ph_reading
 from pioreactor_ph_reading.ph_regulation import *
