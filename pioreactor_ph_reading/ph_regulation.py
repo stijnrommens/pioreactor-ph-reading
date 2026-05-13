d70662354a8765981ca2dd8ed6b5c021dc18d8d6
Stijn Rommens <113170925+stijnrommens@users.noreply.github.com>
2026-05-13 16:59:01 +0200

    Attempt for MQTT communication

--- pioreactor_ph_reading/ph_regulation.py
+++ pioreactor_ph_reading/ph_regulation.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-from pioreactor_ph_reading.ph_reading import PHReading
-
 from pioreactor.automations.dosing.base import DosingAutomationJobContrib
-# from pioreactor.pubsub import subscribe
+from pioreactor.pubsub import subscribe
 
 
 class PHRegulation(DosingAutomationJobContrib):
@@ -21,7 +19,12 @@ class PHRegulation(DosingAutomationJobContrib):
         self.target_ph     = float(target_ph)
 
     def execute(self):
-        if PHReading.ph < (self.target_ph - 0.1):
+        actual_ph = subscribe(f"pioreactor/{self.unit}/{self.experiment}/{self.job_name}/ph_reading/ph", timeout=1)
+        if actual_ph is not None:
+            actual_ph = actual_ph.payload.decode() # this is now a string, you may need to call float(...) etc to convert it. 
+        else:
+            actual_ph = None
+        if actual_ph < (self.target_ph - 0.1):
             vol = self.add_alt_media_to_bioreactor(
                 ml=self.dosing_volume,
                 source_of_event=f"{self.job_name}:{self.automation_name}",
