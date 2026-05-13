f70b80c1186d6ef3f855cdd06d2de6e72df14c9d
Stijn Rommens <113170925+stijnrommens@users.noreply.github.com>
2026-05-13 16:42:46 +0200

    Update ph_regulation.py

--- pioreactor_ph_reading/ph_regulation.py
+++ pioreactor_ph_reading/ph_regulation.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
+from pioreactor_ph_reading.ph_reading import PHReading
+
 from pioreactor.automations.dosing.base import DosingAutomationJobContrib
 # from pioreactor.pubsub import subscribe
 
@@ -12,14 +14,15 @@ class PHRegulation(DosingAutomationJobContrib):
         "target_ph": {"datatype": "float", "settable": True, "unit": "-"}
     }
 
-    def __init__(self, dosing_volume, target_ph, **kwargs):
+    def __init__(self, dosing_volume, target_ph, actual_ph, **kwargs):
         super().__init__(**kwargs)
         self.logger.warning("When using pH control, no liquid is removed. Carefully monitor the level of liquid to avoid overflow!")
         self.dosing_volume = float(dosing_volume)
         self.target_ph     = float(target_ph)
+        self.actual_ph     = float(actual_ph)
 
     def execute(self):
-        if self.ph_readings < (self.target_ph - 0.1):
+        if PHReading.ph < (self.target_ph - 0.1):
             vol = self.add_alt_media_to_bioreactor(
                 ml=self.dosing_volume,
                 source_of_event=f"{self.job_name}:{self.automation_name}",
