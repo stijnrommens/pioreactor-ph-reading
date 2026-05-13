4b24d5ebf2d60bf29447159811f777f0fd524f8e
Stijn Rommens <113170925+stijnrommens@users.noreply.github.com>
2026-05-13 13:57:36 +0200

    Add pH control ad DosingAutomation

--- pioreactor_ph_reading/__init__.py
+++ pioreactor_ph_reading/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from pioreactor_ph_reading.base_pump import click_base_pump
 from pioreactor_ph_reading.ph_reading import click_ph_reading
-from pioreactor_ph_reading.ph_regulation import click_ph_regulation
+from pioreactor_ph_reading.ph_regulation import *
--- pioreactor_ph_reading/ph_regulation.py
+++ pioreactor_ph_reading/ph_regulation.py
@@ -1,65 +1,38 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-import click
-from pioreactor_ph_reading.base_pump import BasePump, click_base_pump
 from pioreactor_ph_reading.ph_reading import PHReading, click_ph_reading
 
-from pioreactor.background_jobs.base import BackgroundJobContrib
-from pioreactor.utils.streaming_calculations import PID
-from pioreactor.config import config
-from pioreactor.cli.run import run
-from pioreactor.utils import clamp
-from pioreactor.whoami import get_assigned_experiment_name
-from pioreactor.whoami import get_unit_name
+from pioreactor.automations.dosing.base import DosingAutomationJobContrib
 
 
-class PHRegulation(BackgroundJobContrib):
-    job_name = "ph_regulation"
+# __all__ = ['PHRegulation']
+
+
+class PHRegulation(DosingAutomationJobContrib):
+    automation_name = "ph_regulation"
     published_settings = {
-        "ph_setpoint": {"datatype": "float", "unit": "-", "settable": True},
+        "dosing_volume": {"datatype": "float", "settable": True, "unit": "mL"},
+        "target_ph": {"datatype": "float", "settable": True, "unit": "-"}
     }
 
-    def __init__(self, ph_setpoint:float, **kwargs) -> None:
+    def __init__(self, dosing_volume, target_ph, **kwargs):
         super().__init__(**kwargs)
-        self.logger.warning("When supplying base, no liquid is removed. Carefully monitor the level of liquid to avoid overflow!")
-        # self.pid = PID(
-        #     Kp=config.getfloat("ph_regulation.config", "Kp"),
-        #     Ki=config.getfloat("ph_regulation.config", "Ki"),
-        #     Kd=config.getfloat("ph_regulation.config", "Kd"),
-        #     setpoint=None,
-        #     unit=self.unit,
-        #     experiment=self.experiment,
-        #     job_name=self.job_name,
-        #     target_name="ph",
-        #     output_limits=(-2.5, 2.5),
-        #     derivative_smoothing=0.925,
-        #     pub_client=self.pub_client,
-        # )
-        self.set_ph_setpoint(ph_setpoint)
-
-    def _clamp_ph_setpoint(self, ph_setpoint: float) -> float:
-        return clamp(0.0, ph_setpoint, 14.0)
-    
-    def set_ph_setpoint(self, ph_setpoint:float) -> None:
-        ph_setpoint = float(ph_setpoint)
-        self.ph_setpoint = self._clamp_ph_setpoint(ph_setpoint)
-        # self.pid.set_setpoint(self.ph_setpoint)
+        self.logger.warning(
+            "When using the fed-batch automation, no liquid is removed. Carefully monitor the level of liquid to avoid overflow!"
+        )
+        self.dosing_volume = float(dosing_volume)
+        self.target_ph = float(target_ph)
 
     def execute(self):
-        if self.click_ph_reading() < self.ph_setpoint:
-            self.click_base_pump()
-
-
-
-@run.command(name="ph_regulation")
-def click_ph_regulation() -> None:
-    """
-    Controls pH by automatic base dosing.
-    """
-    unit = get_unit_name()
-    job = PHRegulation(
-        unit=unit,
-        experiment=get_assigned_experiment_name(unit),
-    )
-    job.execute()
+        if self.click_ph_reading() < self.target_ph:
+            vol = self.add_media_to_bioreactor(
+                ml=self.dosing_volume,
+                source_of_event=f"{self.job_name}:{self.automation_name}",
+                unit=self.unit,
+                experiment=self.experiment,
+                mqtt_client=self.pub_client,
+                logger=self.logger,
+            )
+            if vol != self.dosing_volume:
+                self.logger.warning("Under-dosed!")
